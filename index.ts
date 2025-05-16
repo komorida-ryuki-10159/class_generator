5208dae122f20ee3bfb09532ff2cd3dd3c0d5275
小森田龍樹 <602-student014@la-study.com>
2025-05-16 11:44:58 +0900

    feat: cliを実装

--- index.ts
+++ index.ts
@@ -1,3 +1,7 @@
+import { stdin as input, stdout as output } from "node:process";
+import * as readline from "node:readline";
+import fs from 'fs/promises';
+
 type AccessModifier = 'public' | 'private' | 'protected' | 'package';
 
 interface PhpClass {
@@ -59,3 +63,57 @@ function codeGenerator(c: PhpClass) {
 
   return `${code.use.join('\n')}\n\n${code.class}`;
 }
+
+class ReadLine {
+  private rl = readline.createInterface({ input, output });
+  public question(message: string) {
+    return new Promise<string>((resolve, reject) => {
+      try {
+        this.rl.question(message, (ans) => resolve(ans));
+      } catch (e) {
+        // 握りつぶし
+      }
+    });
+  }
+}
+
+async function main() {
+  const rl = new ReadLine();
+
+  const name = await rl.question('class name > ');
+  const fields: (PhpClassMethod | PhpClassProperty)[] = await rl
+    .question('csv file path >')
+    .then(async path => await fs.readFile(path, 'utf-8'))
+    .then(file => file.split('\n').map(row => {
+      const [name, accessModifier, type, description] = <[string, AccessModifier, string, string | undefined]>row.split(',');
+      const args = name
+        .match(/\(([^)]*)\)/)?.[1]
+        ?.split(',')
+        .filter(arg => arg.length > 0)
+        .map(arg => {
+          const [type, name] = arg.split(/\s+/);
+          return { name, type };
+        });
+
+      return args
+        ? { name: name.replace(/\(.*\)/g, ''), accessModifier, type, args, description }
+        : { name, accessModifier, type, description }
+    }));
+  const impl = await rl
+    .question('implements (none)> ')
+    .then(s => s.trim() === 'none' ? undefined : s.split(''));
+  const exten = await rl
+    .question('extends (none)> ')
+    .then(s => s.trim() === 'none' ? undefined : s);
+
+  const code = codeGenerator({
+    name,
+    fields,
+    implements: impl,
+    extends: exten,
+  });
+
+  await fs.writeFile(`${name}.php`, code);
+}
+
+main();
