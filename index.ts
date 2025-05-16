7149025fd8ba74809e799d09d79b9d606111f2d0
小森田龍樹 <602-student014@la-study.com>
2025-05-16 11:21:44 +0900

    feat: codeGeneratorを実装

--- index.ts
+++ index.ts
@@ -24,3 +24,38 @@ interface PhpClassMethod {
   }[];
   description?: string;
 }
+
+function codeGenerator(c: PhpClass) {
+  let code: {
+    use: string[]
+    class: string;
+  } = {
+    use: [],
+    class: `class ${c.name}`
+  };
+
+  if (c.extends) {
+    code.use.push(`use ${c.extends};`);
+    code.class += ` extends ${c.extends.split('/').slice(-1)[0]}`
+  } else if (c.implements) {
+    code.use.push(...c.implements.map(
+      c => `use ${c};`
+    ));
+    code.class += ` implements ${c.implements.map(v => v.split('/').slice(-1)[0]).join(', ')}`
+  }
+
+  if (c.fields.length) {
+    code.class += `\n{\n`;
+
+    for (const field of c.fields) {
+      code.class += `  ${field.accessModifier} ${field.name}`
+      code.class += 'args' in field
+        ? `(${field.args.map(arg => `${arg.type} ${arg.name}`)}): ${field.type} {}`
+        : `: ${field.type}`;
+    }
+
+    code.class += `\n}\n`;
+  }
+
+  return `${code.use.join('\n')}\n\n${code.class}`;
+}
